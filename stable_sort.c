e6bf24e9dc8abd11eac8cee14252eeaa2a7e14a5
xsun <sunxin89@gmail.com>
2020-12-04 14:46:01 -0800

    ok

--- stable_sort.c
+++ stable_sort.c
@@ -8,13 +8,14 @@
 #include <time.h>
 
 #define MAX_N 20
-#define TEST(arr, n, func, args...){ \
-    int *tmp = (int *)malloc(sizeof(int)*n); \
-    memcpy(tmp, arr, sizeof(int)*n); \
+#define TEST(num, n, func, args...){ \
+    int *arr = (int *)malloc(sizeof(int)*n); \
+    memcpy(arr, num, sizeof(int)*n); \
     printf("%s\n", #func);           \
+    output(arr, n);                  \
     func(args);                      \
-    output(num, n);                                 \
-    free(tmp); \
+    output(arr, n);                                 \
+    free(arr); \
 }
 #define swap(a, b) { \
     __typeof(a) tmp = a;  \
@@ -86,17 +87,62 @@ void output(int *data, int n)
 
 void select_sort(int *data, int n)
 {
-    for (int i =0; i<n; i++){
-        int min = data[i];
+    for (int i =0; i<n-1; i++){
         int p = i;
         for (int j = i+1; j<n; j++){
-            if(data[j] < min){
-                min = data[j];
+            if(data[j] < data[p]){
                 p = j;
             }
         }
         swap(data[p], data[i]);
     }
+}
+/*
+ * 【基准值、partition】
+拿出第1个元素作为基准值
+【尾指针】先从后往前找小于基准值的元素，放到第1个元素位置(已为空)，【头指针】再从前往后找大于基准值的值，放到刚刚空出的位置，循环
+最后头尾指针重合，指向一个空位置，放入基准值
+再对基准值左右两部分分别进行以上操作
+ */
+void quick_sort(int *data, int l, int r)
+{
+    if(l > r){
+        return;
+    }
+    int x = l, y = r, z = data[l];
+    while(x < y){
+        while(x < y && data[y] > z) y--;
+        if(x < y){
+            data[x++] = data[y];
+        }
+        while(x < y && data[x] < z) x++;
+        if(x < y){
+            data[y--] = data[x];
+        }
+    }
+    data[x] = z;
+    quick_sort(data, l, x-1);
+    quick_sort(data, x+1, r);
+    return;
+}
+
+void enhanced_quick_sort(int *data, int l, int r)
+{
+    while(l < r){
+        int x = l, y = r, z = data[(l+r)>>1];
+        do {
+            while (x <= y && data[y] > z) y--;
+            while (x <= y && data[x] < z) x++;
+            if (x <= y) {
+                swap(data[x], data[y]);
+                x++;
+                y--;
+            }
+        }while(x <= y);
+        enhanced_quick_sort(data, x, r); //x will be y+1, y+1 -> r range has been sorted already
+        r = y; //move r to be y, since y+1 -> r already sorted.
+    }
+    return;
 }
 int main()
 {
@@ -106,14 +152,16 @@ int main()
     for (int i = 0; i <MAX_N; i++){
         num[i] = rand()%100;
     }
-    output(num, MAX_N);
+    //output(num, MAX_N);
 
     //insert_sort(num, 10);
     //bubble_sort(num, 20);
-    TEST(num, MAX_N, bubble_sort, num, MAX_N);
-    TEST(num, MAX_N, insert_sort, num, MAX_N);
-    TEST(num, MAX_N, select_sort, num, MAX_N);
-    TEST(num, MAX_N, merge_sort, num, 0, MAX_N-1);
+    TEST(num, MAX_N, bubble_sort, arr, MAX_N);
+    TEST(num, MAX_N, insert_sort, arr, MAX_N);
+    TEST(num, MAX_N, select_sort, arr, MAX_N);
+    TEST(num, MAX_N, merge_sort, arr, 0, MAX_N-1);
+    TEST(num, MAX_N, quick_sort, arr, 0, MAX_N-1);
+    TEST(num, MAX_N, enhanced_quick_sort, arr, 0, MAX_N-1);
     //merge_sort(num, 0, 19);
 
     return 0;
